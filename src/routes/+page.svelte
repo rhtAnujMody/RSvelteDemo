c2ed092d5288859c073372b6367732b037f990b7
rhtAnujMody <anuj.mody@revealhealthtech.com>
2024-07-30 23:28:08 +0530

    fit commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="flex flex-1 w-full h-screen bg-slate-100 flex-col">
+	<h1 class="">Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
